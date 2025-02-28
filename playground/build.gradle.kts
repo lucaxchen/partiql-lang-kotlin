2dffb5e135f5b94c5c3cdbe76f38902f3a77cb7f
Yang Chen <lucaxchen@gmail.com>
2025-02-27 22:46:24 -0800

    change MavenCentral dependencies to local project dependencies

--- playground/build.gradle.kts
+++ playground/build.gradle.kts
@@ -23,12 +23,17 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("org.partiql:partiql-spi:1.1+")
-    implementation("org.partiql:partiql-lang:1.1+")
+//    implementation("org.partiql:partiql-lang:1.1+")
+
+    implementation(project(":partiql-ast"))
+    implementation(project(":partiql-eval"))
+    implementation(project(":partiql-parser", configuration = "shadow"))
+    implementation(project(":partiql-plan"))
+    implementation(project(":partiql-planner"))
+    implementation(project(":partiql-spi"))
+
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.20")
     implementation("com.google.code.gson:gson:2.12.1")
-    implementation("org.apache.httpcomponents.client5:httpclient5:5.4.2")
-    implementation("org.apache.httpcomponents.client5:httpclient5-fluent:5.4.2")
     implementation(platform("software.amazon.awssdk:bom:2.20.100"))
     implementation("software.amazon.awssdk:s3:2.20.100")
 }
